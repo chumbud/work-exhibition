2db1f0887f58ae31521f06de477702073e040fa1
Glitch (work-site) <none>
2020-01-06 21:08:53 +0000

    🕹🎻 starting content generation

--- timeline.js
+++ timeline.js
@@ -6,8 +6,20 @@ function scrollProgress() {
   
 	  var scrollStatePercentage = (container.scrollTop / pageWidth) * 100
 	  document.querySelector("#tracker").style.width = scrollStatePercentage + "%"
-    console.log(scrollStatePercentage)
+    //console.log(scrollStatePercentage)
 	}
 container.addEventListener("scroll", function() {
   setTimeout(scrollProgress(), 500)
+})
+
+let submittedWorks = []
+const workList = document.querySelector("#horizontal-inner-container")
+
+fetch('json/entries.json')
+.then(jsonData => {
+  submittedWorks = jsonData
+    submittedWorks.forEach(piece => {
+      const section = document.createElement("section")
+      section.innerHTML =
+    })
 })
