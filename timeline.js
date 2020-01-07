d7690cb1477bd5148b65d348a7b9ccc8be3c0880
PJ <pj@pjrosa.com>
2020-01-07 11:57:47 -0500

    first test

--- timeline.js
+++ timeline.js
@@ -11,3 +11,21 @@ function scrollProgress() {
 container.addEventListener("scroll", function() {
   setTimeout(scrollProgress(), 500)
 })
+
+let submittedWorks = []
+const workList = document.querySelector("#horizontal-inner-container")
+
+fetch('json/entries.json')
+  .then(jsonData => {
+    let count = 1
+    submittedWorks = jsonData
+    submittedWorks.forEach(piece => {
+      const section = document.createElement("section")
+      section.setAttribute("id", count)
+      section.classList.add("workspace");
+      count++;
+
+      section.innerHTML = "<div class='workspace-container'><grid class='workspace-grid'><div class='workspace-frame'><img src='./assets/user/" + piece.img + "'><h2>" + piece.date + "</h2></div><div class='workspace-sidebar'><p>" + piece.description + "</p><h3>" + piece.author + "</h3><div class='links'></div></div></grid></div>"
+      workList.appendChild(section)
+    })
+  })
